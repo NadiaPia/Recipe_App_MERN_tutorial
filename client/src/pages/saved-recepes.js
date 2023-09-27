85225bf74dc174921e46275ecd84e2434b1c31c8
NadiaPia <206i417@gmail.com>
2023-09-27 11:09:37 -0700

    Add fnctionality to the saved-recipe page

--- client/src/pages/saved-recepes.js
+++ client/src/pages/saved-recepes.js
@@ -1,11 +1,61 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import useGetUserID from "../hooks/useGetUserID";
+
 
 function SavedRecepes() {
+  const [savedRecipes, setSavedRecipes] = useState([]);  
+
+  const userId = useGetUserID();
+
+  useEffect(() => {
+
+    const fetchSavedRecipe = async () => {
+      try {
+      const response = await axios.get(`http://localhost:3001/recipes/savedRecipes/${userId}`);
+      setSavedRecipes(response.data.savedRecipes);  
+      //console.log("response.dataaaa", response.data)  
+      } catch (err) {
+        console.error(err);
+      };
+    };
+
+    fetchSavedRecipe();
+  }, []); 
+
+  
   return (
     <div>
-      SavedRecepes
+      <h1>Saved Recipes</h1>
+
+      <ul>
+        {savedRecipes.map((recipe) => (
+          <li key={recipe._id}>
+            
+            <div>
+              <h2>
+                {recipe.name}
+              </h2>              
+            </div>
+
+            <div className="instructions">
+              <p>
+                {recipe.instructions}
+              </p>
+            </div>
+
+            <img src={recipe.imageUrl} alt={recipe.name}/>
+
+            <p>
+              Cooking Time: {recipe.cookingTime} (minutes)
+            </p>
+
+          </li>)
+        )}
+      </ul>
+      
     </div>
   )
 }
 
-export default SavedRecepes
+export default SavedRecepes;
